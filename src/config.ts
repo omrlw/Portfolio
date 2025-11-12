e421456d1434908a4d0b5cc2e6853cd3aa84123b
omrlw <omr_try@outlook.com>
2025-11-12 10:14:35 -0500

    update

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,12 @@
 export const SITE = {
     website: "https://sebastianportfolio.site/",
     title: "Simple Portfolio",
-    description: "A simple but beautiful portfolio created with Astro",
+    description: "Mi portafolio moderno y sencillo hecho con Astro",
     tags: ["portfolio", "Resume cv", "Astro"],
     ogImage: "/og-image.webp",
     logo: "logo",
     logoText: "Portfolio",
-    lang: "en",
+    lang: "es",
     favicon: "/favicon.png",
     repository: "https://github.com/omrlw",
     author: "Sebastian Chacon",
@@ -20,59 +20,59 @@ export const ME = {
     profileFacts: [
         {
             value: 2,
-            description: "Years of Experience"
+            description: "Años de Experiencia"
         },
         {
             value: 5,
-            description: "Completed Projects"
+            description: "Projectos Completados"
         },
         {
             value: 4,
-            description: "Satisfied Clients"
+            description: "Clientes Satisfechos"
         }
     ],
     contactInfo: {
         email: "omrchacon04@gmail.com",
-        linkedin: "https://www.linkedin.com/in/victor-alvaradohn",
+        linkedin: "https://www.linkedin.com/in/sebastian-chacon-6293a0390/",
         resumeDoc: "Resume_SebastianChacon_2026.pdf",
     },
     aboutMe: "I am a software engineer with a passion for web development. I have experience in building web " + "applications using modern technologies. I am a self-taught developer who enjoys learning new things and " + "sharing knowledge with others.",
 }
 
 export const SOCIALS = [
-    {
-        name: "Twitter",
-        url: "https://twitter.com/vicbox.dev",
-        icon: "twitter-x-fill",
-        show: true
-    },
-    {
-        name: "Facebook",
-        url: "https://www.facebook.com/vicbox.dev",
-        icon: "facebook-fill",
-        show: false
-    },
-    {
-        name: "Instagram",
-        url: "https://www.instagram.com/vicbox.dev",
-        icon: "instagram-fill",
-        show: true
-    },
-    {
-        name: "Youtube",
-        url: "https://www.youtube.com/vicbox.dev",
-        icon: "youtube-fill",
-        show: true
-    },
-    {
-        name: "TikTok",
-        url: "https://tiktok.com/vicbox.dev",
-        icon: "tik-fill",
-        show: false
-    },
+    // {
+    //     name: "Twitter",
+    //     url: "https://twitter.com/vicbox.dev",
+    //     icon: "twitter-x-fill",
+    //     show: true
+    // },
+    // {
+    //     name: "Facebook",
+    //     url: "https://www.facebook.com/vicbox.dev",
+    //     icon: "facebook-fill",
+    //     show: false
+    // },
+    // {
+    //     name: "Instagram",
+    //     url: "https://www.instagram.com/vicbox.dev",
+    //     icon: "instagram-fill",
+    //     show: true
+    // },
+    // {
+    //     name: "Youtube",
+    //     url: "https://www.youtube.com/vicbox.dev",
+    //     icon: "youtube-fill",
+    //     show: true
+    // },
+    // {
+    //     name: "TikTok",
+    //     url: "https://tiktok.com/vicbox.dev",
+    //     icon: "tik-fill",
+    //     show: false
+    // },
     {
         name: "GitHub",
-        url: "https://github.com/vicbox.dev",
+        url: "https://github.com/omrlw",
         icon: "github-fill",
         show: true
     }
