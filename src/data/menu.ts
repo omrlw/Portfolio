637d5af500842d055fcb54691a2a195bbd5eff35
omrlw <omr_try@outlook.com>
2025-11-14 15:04:11 -0500

    Update Blog

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -7,8 +7,8 @@ export const menu = [
         href: '/projects',
         label: 'Proyectos',
     },
-    // {
-    //     href: '/blog',
-    //     label: 'Blog',
-    // },
+    { 
+        href: '/blog',
+        label: 'Blog',
+    },
 ]
