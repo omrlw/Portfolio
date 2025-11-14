f9e40141ee13bd7368d7897d3951271852f77c37
omrlw <omr_try@outlook.com>
2025-11-13 21:24:17 -0500

    update cv

--- src/components/TabsButtons.tsx
+++ src/components/TabsButtons.tsx
@@ -4,8 +4,8 @@ const TabsButtons = (props:any) => {
     return (
         <Tabs defaultValue="about" className="w-full ">
             <TabsList className={"bg-white dark:bg-n700  w-full px-2 py-8 rounded-xl"}>
-                <TabsTrigger className={"w-full py-4 rounded-lg dark:data-[state=active]:text-primary-dark data-[state=active]:bg-light-theme dark:data-[state=active]:bg-dark-theme"} value="about">Sobre Mí</TabsTrigger>
-                <TabsTrigger className={"w-full py-4 rounded-lg dark:data-[state=active]:text-primary-dark data-[state=active]:bg-light-theme dark:data-[state=active]:bg-dark-theme"} value="portfolio">Portfolio</TabsTrigger>
+                <TabsTrigger className={"w-full py-4 rounded-lg dark:data-[state=active]:text-primary-dark font-bold data-[state=active]:bg-light-theme dark:data-[state=active]:bg-dark-theme"} value="about">Sobre Mí</TabsTrigger>
+                <TabsTrigger className={"w-full py-4 rounded-lg dark:data-[state=active]:text-primary-dark font-bold data-[state=active]:bg-light-theme dark:data-[state=active]:bg-dark-theme"} value="portfolio">Portfolio</TabsTrigger>
             </TabsList>
             <TabsContent value="portfolio">
                 {props.portfolio}
